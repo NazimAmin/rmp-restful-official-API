bd6593bc7633711eb6143e10fe4bbb254439e567
Nazim <nazim@Nazims-MBP.T-mobile.com>
2016-08-26 01:56:08 -0400

    rate my professor's official API implimentation

---
+++ main.py
@@ -0,0 +1,104 @@
+#!flask/bin/python
+import json
+import logging
+from google.appengine.api import urlfetch
+from flask import Flask, jsonify, abort, make_response
+
+app = Flask(__name__)
+RMP_BASE_URL = 'http://www.ratemyprofessors.com/api/professors/'
+RMP_HEADERS = {
+        'Content-Type' : 'application/json',
+        'X-Auth-Token' : '20vkiiqon89ioc9o##############',
+    }
+
+def get_prof_id( name ):
+    """
+        Searches the given name
+        :param name: name to search
+        :return: returns id or None
+    """
+    url = "http://search.mtvnservices.com/typeahead/suggest/?solrformat=true&q={0}%2BAND%2Bschoolid_s%3A971&defType=edismax&qf=teacherfullname_t%5E1000%2Bautosuggest&bf=pow%28total_number_of_ratings_i%2C2.1%29&sort=&siteName=rmp&rows=20&start=0&fl=pk_id%2Bteacherfirstname_t%2Bteacherlastname_t%2Btotal_number_of_ratings_i%2Baverageratingscore_rf%2Baverageclarityscore_rf%2Baveragehelpfulscore_rf%2Baverageeasyscore_rf%2Bchili_i%2Bschoolid_s".format(name.replace(' ', '%20'))
+    res = urlfetch.fetch(url, method='GET')
+    pid = json.loads(res.content)
+    pid = pid['response']['docs']
+    return str(pid[0]['pk_id']) if len(pid) > 0 else None
+
+def set_hot ( value ):
+    """
+        calculates the value to be used in 1/2/3 scale
+        :return: 1/2/3
+    """
+    if value is not None:
+        if value < 0:
+            return 1
+        elif 0 <= value <= 50:
+            return 2
+        else:
+            return 3
+        
+def get_ratings ( url ):
+    """
+        Sends a GET request to the given URL
+        :param url: url to request
+        :return: returns JSON resonse 
+            {avgEasiness: 2.5,
+            avgRating: 4.6,
+            chilliPepper: true,
+            dept: "Computer Science",
+            easinessText: "Fair",
+            fName: " FIRST ",
+            fullName: " FIRST LAST ",
+            hotness: 34,
+            id: 000000,
+            lName: " LAST ",
+            latesRatingDate: "2016-08-18T04:00:00Z",
+            middleInitials: "",
+            numOfRatings: 183,
+            ratingText: "Awesome",
+            school: "SOME SCHOOL",
+            status: "Active",
+            wouldTakeAgainPercent: 100
+            }
+    """
+    res = urlfetch.fetch(url, method='GET', headers=RMP_HEADERS)
+    res = json.loads(res.content)
+    if 'hotness' in res: 
+        logging.info(res['hotness'])
+        res['hotness'] = set_hot(res['hotness']) 
+    return res
+    
+@app.route('/professor/<string:name>', methods = ['GET'])
+@app.route('/professor/<string:name>/<path:path>', methods = ['GET'])
+def get_professor_rating( name=None, path=None ):
+    """
+        responds with the requested data
+        :param name: name of the professor
+        :param path: url path
+        :return: ratings or error
+    """
+    if name is not None:
+        try:
+            url = RMP_BASE_URL + get_prof_id( name )
+            if path is not None:
+                if path == 'ratingsbyclass':
+                    url = url + '/ratingsbyclass'
+                elif path == 'ratings':
+                    url = url + '/ratings'
+                else:
+                    abort(404)
+            return jsonify( get_ratings( url ) )
+        except Exception as e:
+            logging.info(e)
+            abort(404)    
+    else:
+        abort(404)
+        
+@app.errorhandler(500)
+def not_found(error):
+    return make_response(jsonify( { 'err': 'Trust me it\'s not you. It\'s the developer!'} ), 500)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'err': 'No professor found that matches the given name' } ), 404)
+if __name__ == '__main__':
+    app.run(debug = True)
